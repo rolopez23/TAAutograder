ac098aab683f1c6b3687850fac438a05a7f0658f
Rob Lopez <rob.lopez@gmail.com>
2020-11-09 14:35:59 -0800

    MVP

--- getCommit.js
+++ getCommit.js
@@ -1,7 +1,7 @@
 const axios = require('axios'); 
 
 const token = require('./token/token.js');
-const findMaxCommit = require('./findMaxCommit.js')
+//const findMaxCommit = require('./findMaxCommit.js')
 
 function getCommitHistory(url){
   const options = {
@@ -12,9 +12,11 @@ function getCommitHistory(url){
   return axios.get(url, options)
 }
 
-getCommitHistory('https://api.github.com/repos/hackreactor/hrsf129-technical-assessment-solutions/commits?sha=yutoliiho')
-  .then((response)=>{
-    console.log(response.data);
-    return findMaxCommit(response.data);
-  })
-  .then(console.log)
+// getCommitHistory('https://api.github.com/repos/hackreactor/hrsf129-technical-assessment-solutions/commits?sha=yutoliiho')
+//   .then((response)=>{
+//     console.log(response.data);
+//     return findMaxCommit(response.data);
+//   })
+//   .then(console.log)
+
+  module.exports = getCommitHistory;
--- savefile.js
+++ savefile.js
@@ -1,22 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 
+const findMaxCommit = require('./findMaxCommit.js')
+const getCommitHistory = require('./getCommit');
+const COHORT = 'hrsf129'
+
+const baseUrl = `https://api.github.com/repos/hackreactor/${COHORT}-technical-assessment-solutions/commits?sha=`
+
 const stringToMatrix = (string) => {
-  array = string.split('/n')
+  array = string.split('\n')
   for (let i = 0; i < array.length; i++) {
-    array[i] = array[i].split(',')
+    const row = array[i];
+
+    array[i] = row.split(',').slice(0, 2)
   }
   return array;
 }
 
-const filePath = path.join(__dirname, '','students.csv'); 
+const filePath = path.join(__dirname, 'students.csv');
 //Read the file
-fs.readFile(filePath, (err, data)=> {
-  if(err) {
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
     console.log(err)
   } else {
-    console.log(data)
-    //console.log(stringToMatrix(data))
+    let studentMatrix = stringToMatrix(data).slice(1);
+    const gitHubRequests = studentMatrix.map(student => getCommitHistory(baseUrl + student[1]))
+    Promise.all(gitHubRequests)
+      .then((responses) => responses.map((response) => {
+        return findMaxCommit(response.data)
+      }))
+      .then(furthestSteps => furthestSteps.map((step, index) => [...studentMatrix[index], step]))
+      .then((studentMatrix)=>{
+        studentMatrix = [['Name','GitHub Handle', 'TA complete to']].concat(studentMatrix);
+        for (let i = 0; i < studentMatrix.length; i++) {
+          studentMatrix[i] = studentMatrix[i].join(',');
+        }
+        console.log(studentMatrix)
+        const text = studentMatrix.join('\n');
+        fs.writeFile('taSteps.csv', text,(err) => {
+          if(err) {
+            console.log('error')
+          } else {
+            console.log('done')
+          }
+        } )
+      })
+      .catch(console.log)
   }
 })
 
